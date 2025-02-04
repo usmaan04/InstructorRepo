16d3433dfc8d26ab5c188ee4a7927c14aa5a42cf
Usmaan Ahmed <158786456+usmaan04@users.noreply.github.com>
2025-02-04 14:39:54 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  // Edit made by YourName_YourStudent_number
+  // Edit made by Usmaan_230117086
   //Last line print('Good bye')
 }
 
